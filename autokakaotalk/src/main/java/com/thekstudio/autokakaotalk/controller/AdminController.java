a36be47b686f7f2e2785e4585f5f8e2f200ac030
kiyong <kiyong@kiyong-PC>
2017-03-22 11:11:51 +0900

    관리자페이지 최초등록

--- autokakaotalk/src/main/java/com/thekstudio/autokakaotalk/controller/AdminController.java
+++ autokakaotalk/src/main/java/com/thekstudio/autokakaotalk/controller/AdminController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles requests for the application home page.
@@ -25,4 +26,30 @@ public class AdminController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
 	
+	/**
+	  * @Method Name : admin
+	  * @작성일 : 2017. 3. 22.
+	  * @작성자 : kiyong
+	  * @변경이력 : 
+	  * @Method 설명 :
+	  * @param param
+	  * @param model
+	  * @return
+	  * @throws Exception
+	  */
+	@RequestMapping(value = "/autokakao/admin", method = RequestMethod.GET)
+	public ModelAndView admin(@RequestParam HashMap<String,Object> param, ModelAndView model) throws Exception {
+		
+//		model.addObject("bjdongList",	addressService.getBjdongList(param));
+//		model.addObject("roadList", 	addressService.getRoadNameList(param));
+		
+		String viewName = "";
+		
+		// JSP페이지
+		viewName = "/admin/admin";
+		
+		model.setViewName(viewName);
+		
+		return model;
+	}
 }
